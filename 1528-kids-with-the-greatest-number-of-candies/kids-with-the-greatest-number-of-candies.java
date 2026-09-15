65592ab540633395e314aa90f98c66754e0aa03e
kirubananthanmec24-alt <kirubananthanm.ec24@bitsathy.ac.in>
2026-09-15 17:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (43.55%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> a=new ArrayList<>();
+        int max=candies[0];
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }}
+            for(int i=0;i<candies.length;i++){
+                if(candies[i]+extraCandies>=max){
+                    a.add(true);
+                }
+                else{a.add(false);}}return a;}}
