46844ab2f1736c991a064e9bb3cc37dd270036f0
kirubananthanmec24-alt <kirubananthanm.ec24@bitsathy.ac.in>
2026-09-25 16:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (68.44%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while(b!=0){
+            int cary=a&b;
+            a=a^b;
+            b=cary<<1;}
+            return a;
+    
+        
+    }
+}
